240f69960cfd15fb4fb39cee7a0473acbe7f05a7
ewang <ewang@ewang-350V5C.(none)>
2013-11-02 16:03:08 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,34 @@
+import wikipedia
+import random
+
+
+def returnRandomLink(linkList):
+    randIndex = random.randint(0,len(linkList)-1)
+    return linkList[randIndex]
+
+
+def getNext(startTopic):
+    startPage = wikipedia.page(startTopic)
+    return returnRandomLink(startPage.links)    
+
+def main():
+    print "Enter Start Page:"
+    startTopic = raw_input()
+    print "Enter Jumps:"
+    x = int(raw_input())
+    currentTopic = startTopic
+
+    topiclist=[]
+
+    for i in xrange(x):
+        topiclist.append(currentTopic)
+        currentTopic = getNext(startTopic)
+    topiclist.append(currentTopic)
+    print "End Topic: " + currentTopic
+    print "GO! Press enter to see answer."
+    
+    raw_input()
+    print topiclist
+
+if __name__ == '__main__':
+    main()
